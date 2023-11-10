550350619c242d11f59600edf7c7d0da09e3a143
etan094 <etan094@gmail.com>
2023-11-10 14:49:50 -0800

    Added routes to home and property listing page.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,8 @@ import type { AppProps } from "next/app"
 import "../styles/globals.css"
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  // return <Component {...pageProps} />
+  return <div>Home</div>
 }
 
 export default MyApp
